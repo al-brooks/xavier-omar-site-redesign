98d7e1e701e8c78c7db43bd07ee5ab72f39c5c7a
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-09-29 15:43:14 -0400

    adding success page after purchase

---
+++ xavier-omar-react/src/components/Success.js
@@ -0,0 +1,10 @@
+const Success = () => {
+  return (
+    <div>
+      <h1>Thank you for your order!</h1>
+      <p>Be on the look out for new merch!</p>
+    </div>
+  );
+};
+
+export default Success;
