2cb3a6ed22e9fa9d0df5e7b1d0bb3210c66b8435
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-10-03 20:06:55 -0400

    finalized main page styling

--- xavier-omar-react/src/components/EmailList.js
+++ xavier-omar-react/src/components/EmailList.js
@@ -1,9 +1,12 @@
 const EmailList = () => {
   return (
-    <form id="emailList" action="http://localhost:8080/email" method="POST">
-      <input type="text" name="email" placeholder="Enter E-mail" required />
-      <button>Submit</button>
-    </form>
+    <section id="emailList">
+      <span id="subscribe">Subscribe For VIP Access</span>
+      <form id="emailForm" action="http://localhost:8080/email" method="POST">
+        <input type="text" name="email" placeholder="Enter E-mail" required />
+        <button type="submit">Submit</button>
+      </form>
+    </section>
   );
 };
 
